d46cbbb6478deab14331e5cb8d864ca8875f0025
mac <mac@MacBook-Pro-mac.local>
2026-04-25 02:06:03 +0700

    -21 adding cookies feature

--- app/Http/Controllers/Api/CommandController.php
+++ app/Http/Controllers/Api/CommandController.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Str;
+use Symfony\Component\Process\Process;
+use Illuminate\Support\Facades\File;
+use App\Models\Token;
 use App\Models\CommandJob;
 use App\Jobs\RunPythonCommandJob;
 
